560335b71f3e4cedae5743ef7fee44f116c8bbca
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-11-27 11:45:56 -0800

    Java if statement and method examples were swapped. Added .gitignore for compiled files

--- if/IfJava.java
+++ if/IfJava.java
@@ -1,16 +1,14 @@
-public class IfJava {
-   public static int test(int param1, int param2, char param3, char param4){
-       System.out.printf("The two characters are %c, %c\n", param3, param4);
-       int answer = param1 + param2;
-       return answer;
-   }
+public class MethodsJava {
    public static void main(String[] args) {
-       int w = 5;
-       int x = 6;
-       char y = 'd';
-       char z = 'c';
-       int ans = test(w,x, y,z);
-       System.out.printf("Adding the two numbers makes %d\n", ans);
+        int x = 5;
+        
+        if (x < 5) {
+        System.out.println("X is less than 5");
+        } else if (x > 5) {
+        System.out.println("x is greater than 5");
+        } else {
+        System.out.println("x is equal to 5");
+        }
    }
 }
 
